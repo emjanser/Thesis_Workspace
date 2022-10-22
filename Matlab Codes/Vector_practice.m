d4fa7aa1746bc6d9ac9c8b2fb6a6db4c26a997a0
Emrecan Serin <75207813+emjanser@users.noreply.github.com>
2022-10-22 15:54:08 +0100

    New Changes

---
+++ Matlab Codes/Vector_practice.m
@@ -0,0 +1,7 @@
+W = [1,2,3,4;5,6,7,8]
+
+x = [100,200]
+
+z = (x.').*W
+
+% Transposing "x" vector makes it possible to multiply it with the "W" matrix.
---
+++ Numpy Practice/Vectors.py
@@ -0,0 +1,7 @@
+import numpy
+
+W = ["x1", "y1", "x2", "y2"],["x1", "y1", "x2", "y2"]
+x = [1,2]
+
+print(W,x)
+
